d49e743e85b8a5a34dccc5b4502a809d37ca9ea5
Leonardo Hernandez <leo.qro@comunidad.unam.mx>
2023-03-28 16:07:31 -0600

    trying to pass CLI arguments to Rscript
    I tried to pass arguments so that this script run with the other and be part of
    the GWAS script

--- assoc_results_plotts.R
+++ assoc_results_plotts.R
@@ -1,13 +1,11 @@
-library("optparse")
- 
-option_list = list(
-  make_option(c("-f", "--file"), type="character", default=NULL, 
-              help="dataset file name", metavar="character"),
-    make_option(c("-o", "--out"), type="character", default="out.txt", 
-              help="output file name [default= %default]", metavar="character")
-); 
-opt_parser = OptionParser(option_list=option_list);
-opt = parse_args(opt_parser);
+args = commandArgs(trailingOnly=TRUE)
+assoc_result_snps<-read.table(args[1], header=TRUE)
+file=args[1]
+print(file)
+assoc_result_snps<-read.table(file, header=TRUE)
+output_for_qq=args[2]
+output_for_man=args[3]
+
 library(lattice)
 manhattan.plot<-function(chr, pos, pvalue, 
                          sig.level=NA, annotate=NULL, ann.default=list(),
