1135a7157dac28ed4377d46a20a2aa61f4626342
Leonardo Hernandez <leo.qro@comunidad.unam.mx>
2023-04-01 10:32:23 -0600

    Added new description in line  12

--- assoc_study.sh
+++ assoc_study.sh
@@ -1,6 +1,6 @@
 #we now have to create a different  directory to save the results of our logistic regression
 mkdir ${outdirectory}${todays_date}_Assoc_results
-input_for_assoc_study=${outdirectory}${todays_date}_QC_for_assoc_study/${todays_date}_output_from_QC
+input_for_assoc_study=${outdirectory}${todays_date}_QC/${todays_date}_generalQC/${todays_date}_output_from_QC
 output_for_Assoc_study=${outdirectory}${todays_date}_Assoc_results/${todays_date}_results_assoc_study
 output_values_of_PCA=${outdirectory}${todays_date}_PCA/ #to be used as covar file
 
@@ -9,7 +9,7 @@ if [ -d "$output_values_of_PCA" ]; then
     covarfile=${outdirectory}${todays_date}_PCA/covarfile.txt
     pheno_file=${outdirectory}${todays_date}_PCA/pheno.txt
 else
-    echo "hild prunning was not made, input file for for QC is: ${input_file}"
+    echo "PCA script was not run, input fies for covars and extrafiles are in: ${path_to_extrafiles}"
     covarfile=${path_to_extrafiles}phenos_last.txt
     pheno_file=${path_to_extrafiles}phenos_last.txt
 fi
