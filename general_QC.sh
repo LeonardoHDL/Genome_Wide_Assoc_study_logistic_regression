6103d696ba2ee5f471a456e934561c5937c76860
Leonardo Hernandez <leo.qro@comunidad.unam.mx>
2023-04-12 13:04:23 -0600

    i added a new line

--- general_QC.sh
+++ general_QC.sh
@@ -111,3 +111,5 @@ echo "Done with QC, files produced:"
 echo ${output_file_for_QC}
 echo "A separate file containing the MAF count of each snp is writed in:"
 echo ${output_for_freq_count}
+
+echo "hola"
